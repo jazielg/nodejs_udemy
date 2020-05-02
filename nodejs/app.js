d2d33dd2a1e40945d03ba0525c10039324de17ef
jaziel-luby <jaziel@n22.com.br>
2020-05-01 21:35:27 -0300

    33 - redirecting requests

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,10 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
     // process.exit();
     const url = req.url;
-    res.setHeader('Content-Type', 'text/html');
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<header><title>Enter Message</title></header>');
@@ -12,6 +12,13 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
+    if(url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<header><title>My First Page</title></header>');
     res.write('<body><h1>Hello from my Node.js server!</h1></body>');
