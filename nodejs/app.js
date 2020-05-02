1f32d088f8b81b8ea32b3d06dd0b9f7aa6104a02
jaziel-luby <jaziel@n22.com.br>
2020-05-01 21:30:54 -0300

    32 - routing requests

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,9 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    // console.log(req.url, req.method, req.headers);
     // process.exit();
+    const url = req.url;
     res.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<header><title>Enter Message</title></header>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.write('<html>');
     res.write('<header><title>My First Page</title></header>');
     res.write('<body><h1>Hello from my Node.js server!</h1></body>');
