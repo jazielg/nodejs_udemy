73f58b4faaa563ecc307cc366e4767702f27484c
jaziel-luby <jaziel@n22.com.br>
2020-05-01 21:23:51 -0300

    30 - sending responses

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,6 +3,12 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<header><title>My First Page</title></header>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
