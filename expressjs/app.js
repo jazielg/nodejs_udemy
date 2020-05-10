76489f8d87d379dc87555a35a9b3661a9238fbc2
jaziel-luby <jaziel@n22.com.br>
2020-05-10 16:05:33 -0300

    87 - working with handlebars

--- expressjs/app.js
+++ expressjs/app.js
@@ -3,15 +3,25 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const expressHbs = require('express-handlebars');
+
 const app = express();
 
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
+
+app.engine('hbs', expressHbs({
+    extname: "hbs",
+    defaultLayout: "",
+    layoutsDir: "",
+}));
+app.set('view engine', 'hbs');
+
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
@@ -19,7 +29,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', {pageTitle: 'Page not found'});
+    res.status(404).render('404', { pageTitle: 'Page not found' });
 });
 
 app.listen(3000);
