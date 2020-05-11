647f69dc919136ce03c7f29eb427d5d531f6359d
jaziel-luby <jaziel@n22.com.br>
2020-05-11 20:39:26 -0300

    98 - finishing the controllers

--- expressjs/app.js
+++ expressjs/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 // const expressHbs = require('express-handlebars');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -29,9 +30,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: 'Page not found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
