c212ff0fbedba1c3ce88fcffb4702cc70edf69a7
jaziel-luby <jaziel@n22.com.br>
2020-05-11 21:13:40 -0300

    101 - fetching data from files via the model

--- expressjs/controllers/products.js
+++ expressjs/controllers/products.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
     res.render('add-product', {
-        pageTitle: 'Add Product', 
+        pageTitle: 'Add Product',
         path: '/admin/add-product',
         formsCSS: true,
         productCSS: true,
@@ -19,13 +19,14 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
 }
