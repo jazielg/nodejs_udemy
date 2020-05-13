2f74fe5c8cfb67522d291ac9198afcee188b5815
jazielg <jazielgj1995@gmail.com>
2020-05-12 22:51:09 -0300

    109 - storing product data

--- expressjs/controllers/admin.js
+++ expressjs/controllers/admin.js
@@ -12,6 +12,11 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+
     const product = new Product(req.body.title);
     product.save();
     res.redirect('/');
