dd00af4b1772f61ea46750cc66bdc1f4df1a3ed3
jazielg <jazielgj1995@gmail.com>
2020-05-29 22:17:53 -0300

    141 - retrieving data

--- expressjs/app.js
+++ expressjs/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 // const expressHbs = require('express-handlebars');
 const errorController = require('./controllers/error');
-const db = require('./util/database');s
+const db = require('./util/database');
 
 const app = express();
 
@@ -15,6 +15,10 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+  .then(result => console.log(result[0], result[1]))
+  .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
